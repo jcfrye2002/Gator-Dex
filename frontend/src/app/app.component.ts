1b50f67b31566e741fbf66a5adf7b7886e862ace
samstjean <92886388+samstjean@users.noreply.github.com>
2023-02-28 13:33:04 -0500

    branding

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Go Lang + Angular + Mongo DB CRUD ';
+  title = 'Gator-Dex';
 }
