682d55303b3aa4e625f8ba477e053ba0271e6a0f
juanveliz76 <92879629+juanveliz76@users.noreply.github.com>
2023-04-18 15:55:43 -0400

    Include Gatorcard and GatorDex Class

---
+++ frontend/src/app/gatorCard.ts
@@ -0,0 +1,5 @@
+export class GatorCard {
+    gatorDeckId: string;
+    Question: string;
+    Answer: string;
+}
---
+++ frontend/src/app/gatordex.ts
@@ -0,0 +1,6 @@
+export class Deck {
+    id:string;
+    gatordeckID:string;
+    gatordeckName:string;
+    classCode:string;
+}
