6f676c6542d96fd59adfec74add5a2e2a6840742
samstjean <92886388+samstjean@users.noreply.github.com>
2023-04-18 16:25:45 -0400

    most updated frontend

--- frontend/src/app/create-dex/create-dex.component.ts
+++ frontend/src/app/create-dex/create-dex.component.ts
@@ -5,6 +5,7 @@ import { User } from '../user';
 import { Router } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { Deck } from '../gatordex';
 
 @Component({
   selector: 'app-create-dex',
@@ -19,21 +20,39 @@ export class CreateDexComponent {
   cardForm: FormGroup<any>;
 
   submitted = false;
-
+  deck: Deck = new Deck();
+  
     ngOnInit(){
       this.cardForm = this.fb.group({
         front: ['', Validators.required],
         back: ['', Validators.required]
       });
     }
-
+    newDeck (): void{
+      this.submitted = false;
+      this.deck = new Deck();
+    }
+    save() {
+      this.userService
+      .createUser(this.deck).subscribe(data => {
+        console.log(data)
+        this.deck = new Deck();
+        this.gotoList();
+      }, 
+      error => console.log(error));
+    }
     onSubmit(){
       this.submitted = true;
+      this.save();
       this.router.navigate(['/home']);
     }
 
     clickAddCard(){
       location.reload();
+    }
+
+    gotoList() {
+      this.router.navigate(['/decks']);
     }
 
     clickHome(){
